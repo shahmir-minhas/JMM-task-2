0f3382d043d3e971745a4fe4af1fd81c7c4c2a55
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2021-10-25 13:04:07 +0500

    commitoct25
    sign-up page, search, form validation

--- script/index.js
+++ script/index.js
@@ -1,39 +1,36 @@
 window.onclick = function(event) {
 
-  console.log(event.target);
   if (!event.target.matches('.abc')) {
 
-      var openDropdown = document.getElementById("myDropdown");
-        openDropdown.style.display = "none";
+    var openDropdown = document.getElementById("myDropdown");
+    openDropdown.style.display = "none";
+  }
+
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+
+  if (!event.target.matches('.search') && !event.target.matches('.unsplash-input')) {
+    document.getElementById("search").style.display = "none";
   }
 }
+
 function myFunction() {
   document.getElementById("myDropdown").style.display = "block";
-  console.log("block");
+}
+
+function showSearch() {
+  document.getElementById("search").style.display = "block";
 }
 
 
 var modal = document.getElementById("loginModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("login-btn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close-model")[0];
-
-// When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
